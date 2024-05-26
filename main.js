e6dd8b91c08d62a79a21307f0a2e483faf535a55
milanaismail <133760937+milanaismail@users.noreply.github.com>
2024-05-26 21:34:27 +0200

    animations

--- main.js
+++ main.js
@@ -11,12 +11,14 @@ document.addEventListener('DOMContentLoaded', function() {
     flower.addEventListener('mouseover', function() {
         flower.src = 'images/logo.png'; // Change image source on hover
         flower.style.width = '120px'; 
+        flower.classList.add('no-rotate'); 
         headerContainer.style.backgroundColor = '#F9F9F9'; // Change background color on hover
     });
 
     flower.addEventListener('mouseout', function() {
         flower.src = 'images/flower.png'; // Revert image source when not hovered
         flower.style.width = '50px';
+        flower.classList.remove('no-rotate'); 
         headerContainer.style.backgroundColor = '#EAD2FF'; // Revert background color when not hovered
     });
 });
